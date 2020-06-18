722ce80904fe54894915ef87f572417b41dc27f4
vitalygorbachev <imbarus@gmail.com>
2020-06-18 09:44:15 +0300

    First release

--- src/main/kotlin/com/justai/jaicf/template/Server.kt
+++ src/main/kotlin/com/justai/jaicf/template/Server.kt
@@ -8,12 +8,12 @@ import io.ktor.server.netty.Netty
 import com.justai.jaicf.channel.telegram.TelegramChannel
 
 fun main() {
-    embeddedServer(Netty, 8080) {
-        routing {
-            httpBotRouting(
-                "/" to ActionsFulfillment.dialogflow(templateBot)
-            )
-        }
-    }.start(wait = true)
+//    embeddedServer(Netty, 8080) {
+//        routing {
+//            httpBotRouting(
+//                "/" to ActionsFulfillment.dialogflow(templateBot)
+//            )
+//        }
+//    }.start(wait = true)
     TelegramChannel(templateBot, "1106535183:AAFAJY8wr5tkV9t7E4BTct8WbbZ2BGaiLXk").run()
 }
